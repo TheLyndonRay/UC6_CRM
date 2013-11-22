b317ee5e8cd07e7709975486e9b53a7216456b01
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-22 08:15:26 -0600

    Created UC6_CRM Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UC6Crm::Application.config.secret_token = '74c796ba178d403bbb482f72af7e8d5a63bca0e1adf0c7cf2627e6d06487b5a19cbf5a055df309a4574e90d271f41b3c46de3b8b4c2c12e74dea54af389cd37b'
