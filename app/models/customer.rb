ab034dd13746f223770c04ace9fc37043a696d71
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2013-11-22 09:14:53 -0600

    Added Customer partial and a finder controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :email_address, :full_name, :image, :notes, :phone_number
+
+  validates :full_name, :presence => :true
+  validates :image, :presence => :true
 end
